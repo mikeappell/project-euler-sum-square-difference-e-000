cf9077908f6885b681d312892e17d66f0a440a93
mikeappell <mikeappell@gmail.com>
2015-11-06 05:56:06 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,11 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(num)
+    @num = num
+  end
+  
+  def difference
+    sum_squares = (1..@num).inject(0) {|sum, square| sum += square ** 2}
+    square_sums = ((1..@num).inject(:+)) ** 2
+    (square_sums - sum_squares).abs
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,5 @@
-# Implement your procedural solution here!
+def sum_square_difference(num)
+  sum_squares = (1..num).inject(0) {|sum, square| sum += square ** 2}
+  square_sums = ((1..num).inject(:+)) ** 2
+  (square_sums - sum_squares).abs
+end
